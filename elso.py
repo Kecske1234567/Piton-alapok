3295265b7151e45accf465a26f0c715d11e2806a
Kecske1234567 <bodi.laszlo@szbiszeged.hu>
2022-09-19 10:36:51 +0200

    negyedik

--- elso.py
+++ elso.py
@@ -2,3 +2,11 @@ from os import system
 
 system("cls")
 
+# 2. feladat: prog_alapf.3
+
+#explicit típuskonverzió
+szam = int(input("kérek egy számot:"))
+
+kiir = f"a szám: {szam} és kétszerese: {2 * szam}"
+
+print(kiir)
---
+++ feladatok.py
@@ -0,0 +1,6 @@
+#1. feladat: prog_alapf.2
+
+nev = input("Kérem a nevedet: ")
+koszones = f"hello {nev}!"
+
+print(koszones)
