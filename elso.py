973866ea9956ff0ae8d3eabc7da6f3d128c64c72
Kecske1234567 <bodi.laszlo@szbiszeged.hu>
2022-09-19 10:12:28 +0200

    harmadik

--- elso.py
+++ elso.py
@@ -2,8 +2,3 @@ from os import system
 
 system("cls")
 
-elsoValtozo = 12
-
-kiir = "A változó értéke: " + str(elsoValtozo)
-
-print(kiir)
---
+++ tallon_2.py
@@ -0,0 +1,17 @@
+elsoValtozo = 12
+
+# string konkatenáció
+kiir_1 = "A változó értéke: " + str(elsoValtozo)
+print(kiir_1)
+
+# formattál sztring 1.verzió
+kiir_1 = "A változó értéke: {}".format(elsoValtozo)
+print(kiir_1)
+
+#formattált sztring 2.verzió
+kiir_1 = f"A változó értéke: {elsoValtozo}"
+print(kiir_1)
+
+#formattált sztring 1.verzió
+kiir_1 = "A változó értéke: {0}".format(elsoValtozo)
+print(kiir_1)
